9ecbb43e88b6425e1127aa3ae45052926e1f5aff
BacharKabalan <basharkabalan@gmail.com>
2024-03-24 21:05:14 +0000

    added the google search results to the flow. I also added date column for both my_database and questions tables and updated the streamlit app accordingly.

--- MLOPs/hacker_news/hckr_nws_flow.py
+++ MLOPs/hacker_news/hckr_nws_flow.py
@@ -4,6 +4,9 @@ from prefect import flow, task
 import re
 import psycopg2
 import json
+from serpapi import search
+import pandas as pd
+import datetime
 
 
 @flow(name = 'Daily digest')
@@ -17,7 +20,119 @@ def daily_digest():
 
 @flow(flow_run_name = 'google_mlops_job_search')
 def google_mlops_job_search():
-    print('hello')
+    
+    search_results, google_query = top_10_google_results()
+
+    titles = [result['title'] for result in search_results['organic_results']]
+    title_links = [result['link'] for result in search_results['organic_results']]
+    job_numbers_per_result = find_numbers(titles)
+    data = {google_query: titles,'job_count': job_numbers_per_result, 'title_links':title_links} # 
+    top_10_google_search_results_df = pd.DataFrame(data)    
+
+    conn_params = 'postgres://postgres:mysecretpassword@localhost:5434/postgres'
+    create_sql_table_google_results(conn_params)
+    # Get today's date
+    current_date = datetime.date.today()
+
+    # Iterate over DataFrame rows and insert each row into the SQL table
+    for index, row in top_10_google_search_results_df.iterrows():
+        insert_row_into_table(conn_params, current_date, row)
+
+@task
+def top_10_google_results():
+    params = {
+    "engine": "google",
+    "q": "glassdoor mlops engineer jobs",
+    "location": "United Kingdom",
+    "hl": "en",
+    "gl": "uk",
+    "google_domain": "google.com",
+    "num": "10",
+    "start": "10",
+    "safe": "active",
+    "api_key": "c5c1ab2e9b960344525d642036948de7e7ed02e62c28ff06d3b4b0c3356ff9bd"
+    }
+    
+    search_results = search(params)
+    google_query = params['q']
+    return search_results, google_query
+
+@task
+def find_numbers(strings):
+    numbers = []
+    for string in strings:
+        # Use regular expression to find all numbers in the string
+        found_numbers = re.findall(r'\d+\s', string)
+        # Convert the found numbers from strings to integers and add them to the list
+        
+        if found_numbers:
+            first_number = [found_numbers[0]]
+            if first_number[0] != '2024':
+                numbers.extend(map(int, first_number))
+            else: 
+                numbers.extend(map(int, '0'))
+        else:
+            numbers.extend(map(int, '0'))
+    return numbers
+
+@task
+def create_sql_table_google_results(conn_params):
+    """
+    Task to create a table if it doesn't exist.
+    """
+    try:
+        with psycopg2.connect(conn_params) as conn:
+            with conn.cursor() as cur:
+                create_table_query = """
+                CREATE TABLE IF NOT EXISTS my_dataframe (
+                id SERIAL PRIMARY KEY,
+                insertion_date DATE,
+                search_result_title VARCHAR(255),
+                job_count INTEGER,
+                title_links TEXT
+                );
+                """
+                cur.execute(create_table_query)
+                conn.commit()
+    except Exception as e:
+        print(f"Error creating table: {e}")
+
+
+
+
+@task
+def insert_row_into_table(conn_params, current_date, row):
+    try:
+        with psycopg2.connect(conn_params) as conn:
+            with conn.cursor() as cur:
+                # Check if the question already exists in the table
+                question_exists_query = """
+                SELECT EXISTS(SELECT 1 FROM my_dataframe WHERE search_result_title = %s)
+                """
+                cur.execute(question_exists_query, (row[0],))
+                question_exists = cur.fetchone()[0]
+                if not question_exists:
+                    # If the question does not exist, then insert it
+                    insert_query = """
+                    INSERT INTO my_dataframe (insertion_date, search_result_title, job_count, title_links)
+                    VALUES (%s, %s, %s, %s)
+                    """
+                    cur.execute(insert_query, (current_date, ) + tuple(row))
+                    conn.commit()
+                else:
+                    print("Question already exists in the table, skipping insertion.")
+    except Exception as e:
+        print(f"Error inserting row into SQL table: {e}")
+
+
+
+
+
+
+
+
+
+
 
 @flow(flow_run_name='Fetch Today\'s HN Questions')
 def fetch_all_HN_questions():  
@@ -89,6 +204,7 @@ def create_table(conn_params):
                 create_table_query = """
                 CREATE TABLE IF NOT EXISTS questions (
                 id SERIAL PRIMARY KEY,
+                insertion_date DATE,
                 question VARCHAR(255),
                 age VARCHAR(255),
                 comments TEXT
@@ -104,18 +220,20 @@ def create_table(conn_params):
 def insert_question(conn_params, question, age, comments):
     '''Task to insert a new question into the table.'''
     try:
+        current_date = datetime.date.today()  # Get today's date
         with psycopg2.connect(conn_params) as conn:
             with conn.cursor() as cur:
-                insert_query = """
-                INSERT INTO questions (question, age, comments) VALUES (%s, %s, %s)
-                """
-                ##### Extract text content from BeautifulSoup Tag objects #####
-                # question_text = question.get_text(separator=' ', strip=True)
-                # age_text = age.get_text(separator=' ', strip=True)
-                # Combine comments into a single string
-                comments_text = ' '.join(comments)
-                cur.execute(insert_query, (question, age, comments_text))
-                conn.commit()
+                # Check if the question already exists
+                check_query = "SELECT COUNT(*) FROM questions WHERE question = %s"
+                cur.execute(check_query, (question,))
+                count = cur.fetchone()[0]
+                if count == 0:  # Question doesn't exist, insert it
+                    insert_query = """
+                    INSERT INTO questions (insertion_date, question, age, comments) VALUES (%s, %s, %s, %s)
+                    """
+                    comments_text = ' '.join(comments)
+                    cur.execute(insert_query, (current_date, question, age, comments_text))
+                    conn.commit()
     except Exception as e:
         print(f"Error inserting question: {e}")
 
--- MLOPs/hacker_news/wordcloud_app.py
+++ MLOPs/hacker_news/wordcloud_app.py
@@ -5,6 +5,8 @@ import psycopg2
 import pandas as pd
 from serpapi import search
 import re
+import datetime
+
 
 # Function to fetch text data from PostgreSQL database
 def fetch_data_from_database():
@@ -51,122 +53,71 @@ def generate_wordcloud(data):
 #     "safe": "active",
 #     "api_key": "c5c1ab2e9b960344525d642036948de7e7ed02e62c28ff06d3b4b0c3356ff9bd"
 #     }
-
-#     results = search(params)
-#     titles = [result['title'] for result in results['organic_results']]
-#     title_links = [result['link'] for result in results['organic_results']]
-
-
-#     def find_numbers(strings):
-#         numbers = []
-#         for string in strings:
-#             # Use regular expression to find all numbers in the string
-#             found_numbers = re.findall(r'\d+', string)
-#             # Convert the found numbers from strings to integers and add them to the list
-#             numbers.extend(map(int, found_numbers))
-#         return numbers
-
-#     # Example list of strings
-
-#     # Find all numbers in the list of strings
-#     result = find_numbers(titles)
-#     data = {params['q']: titles, 'title_links':title_links}
-#     df = pd.DataFrame(data)
-#     return df
-
-# def google_job_search():
-#     params = {
-#     "engine": "google",
-#     "q": "glassdoor mlops engineer jobs",
-#     "location": "United Kingdom",
-#     "hl": "en",
-#     "gl": "uk",
-#     "google_domain": "google.com",
-#     "num": "20",
-#     "start": "10",
-#     "safe": "active",
-#     "api_key": "c5c1ab2e9b960344525d642036948de7e7ed02e62c28ff06d3b4b0c3356ff9bd"
-#     }
-#     def find_numbers(strings):
-#         numbers = []
-#         for string in strings:
-#             # Use regular expression to find all numbers in the string
-#             found_numbers = re.findall(r'\d+', string)
-#             # Convert the found numbers from strings to integers and add them to the list
-#             if found_numbers:
-#                 numbers.extend(map(int, found_numbers))
-#             else:
-#                 numbers.extend(map(int, '0'))
-#         return numbers
-#     results = search(params)
-#     titles = [result['title'] for result in results['organic_results']]
-#     title_links = [result['link'] for result in results['organic_results']]
-#     job_numbers_per_result = find_numbers(titles)
-#     data = {params['q']: titles, 'job_count': job_numbers_per_result, 'title_links':title_links}
-#     df = pd.DataFrame(data)
-#     return df
-
-def google_job_search():
-    params = {
-    "engine": "google",
-    "q": "glassdoor mlops engineer jobs",
-    "location": "United Kingdom",
-    "hl": "en",
-    "gl": "uk",
-    "google_domain": "google.com",
-    "num": "20",
-    "start": "10",
-    "safe": "active",
-    "api_key": "c5c1ab2e9b960344525d642036948de7e7ed02e62c28ff06d3b4b0c3356ff9bd"
-    }
     
-    search_results = search(params)
-    google_query = params['q']
-    return search_results, google_query
+#     search_results = search(params)
+#     google_query = params['q']
+#     return search_results, google_query
 
-def find_numbers(strings):
-    numbers = []
-    for string in strings:
-        # Use regular expression to find all numbers in the string
-        found_numbers = re.findall(r'\d+\s', string)
-        # Convert the found numbers from strings to integers and add them to the list
+# def find_numbers(strings):
+#     numbers = []
+#     for string in strings:
+#         # Use regular expression to find all numbers in the string
+#         found_numbers = re.findall(r'\d+\s', string)
+#         # Convert the found numbers from strings to integers and add them to the list
         
-        if found_numbers:
-            first_number = [found_numbers[0]]
-            if first_number[0] != '2024':
-                numbers.extend(map(int, first_number))
-            else: 
-                numbers.extend(map(int, '0'))
-        else:
-            numbers.extend(map(int, '0'))
-    return numbers
-def put_google_search_results_in_df(search_results, google_query):
+#         if found_numbers:
+#             first_number = [found_numbers[0]]
+#             if first_number[0] != '2024':
+#                 numbers.extend(map(int, first_number))
+#             else: 
+#                 numbers.extend(map(int, '0'))
+#         else:
+#             numbers.extend(map(int, '0'))
+#     return numbers
+# def put_google_search_results_in_df(search_results, google_query):
     
-    titles = [result['title'] for result in search_results['organic_results']]
-    title_links = [result['link'] for result in search_results['organic_results']]
-    job_numbers_per_result = find_numbers(titles)
-    data = {google_query: titles,'job_count': job_numbers_per_result, 'title_links':title_links} # 
-    df = pd.DataFrame(data)
-    return df
+#     titles = [result['title'] for result in search_results['organic_results']]
+#     title_links = [result['link'] for result in search_results['organic_results']]
+#     job_numbers_per_result = find_numbers(titles)
+#     data = {google_query: titles,'job_count': job_numbers_per_result, 'title_links':title_links} # 
+#     df = pd.DataFrame(data)
+#     return df
 
 
+def query_table():
+    """
+    Task to query the SQL table and retrieve its contents.
+    """
+    conn_params = 'postgres://postgres:mysecretpassword@localhost:5434/postgres'
 
-    # Example list of strings
+    try:
+        with psycopg2.connect(conn_params) as conn:
+            with conn.cursor() as cur:
+                query = "SELECT * FROM my_dataframe ;"
+                cur.execute(query)
+                rows = cur.fetchall()
+                columns = [desc[0] for desc in cur.description]
+                df = pd.DataFrame(rows, columns=columns)
+                return df
+    except Exception as e:
+        print(f"Error querying table: {e}")
+        return None
 
-    # Find all numbers in the list of strings
+
 
 
 
 
 def main():
-    st.title("Word Cloud from PostgreSQL Database")
+    st.title("Word Cloud from Hacker News in the past 24 hours")
     
     # Fetch data from database
     data = fetch_data_from_database()
     data = data[:90]
-    search, google_query = google_job_search()
-    top_10_google_search_results_df = put_google_search_results_in_df(search, google_query)
-
+    # search, google_query = google_job_search()
+    # top_10_google_search_results_df = put_google_search_results_in_df(search, google_query)
+    # Query the table
+    top_10_google_search_results_df = query_table()
 
     if not data:
         st.warning("No data fetched from the database.")
